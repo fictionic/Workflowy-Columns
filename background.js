f5781e0ce3eca52a8cb2210ca64d7ee17bd5c69b
Dylan Forbes <dylandforbes@gmail.com>
2017-11-02 14:58:33 -0700

    more icons

--- background.js
+++ background.js
@@ -13,4 +13,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: message}, null);
   });
+  // toggle icon
+  if (useColumns) {
+    chrome.browserAction.setIcon({ path: 'icons/icon_38.png' });
+  } else {
+    chrome.browserAction.setIcon({ path: 'icons/inactive_38.png' });
+  }
 });
